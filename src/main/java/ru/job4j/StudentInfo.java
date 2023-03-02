cdc4cdf0ca1ad8c7ee2728b1b8c2f75068fcdcb5
Nikita Gromov <nikita.gromow.2000@gmail.com>
2023-03-02 13:12:08 +0300

    4.1. Разбор первой программы. [#159033 #390361]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Nikita Gromov");
+        System.out.println("02.03.2023");
+
+    }
+}
