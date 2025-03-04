912ad3ec72a776ad7c6f73e51e4548188a8c64c1
FaneD1 <nikita.gromow.2000@gmail.com>
2025-03-04 11:23:04 +0300

    Разбор первой программы по "взрослому" [#505151]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Nikita Gromov");
+        System.out.println("04.03.2025");
+    }
+}
